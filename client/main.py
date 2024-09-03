57f2eb0db9ce545fda9edf5a20291ab722271e61
aaronv <41397746+aaron70@users.noreply.github.com>
2024-09-02 23:58:47 -0600

    Finishing k8s example

--- client/main.py
+++ client/main.py
@@ -2,7 +2,6 @@ import requests, time
 
 if __name__ == "__main__":
     while True:
-        # url = "http://localhost:5000/api/send"
         URL = "http://server-svc.default.svc.cluster.local:80/api/send"
         requests.get(URL)
         time.sleep(5)
